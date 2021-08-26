97ebc546da3ce46cbcb3eb4c87d7f3c1c93e4668
Aaron Taylor <aaron@aphid.io>
2021-08-26 04:15:10 -0700

    show game after sign in

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-Moralis.initialize("INSERT_APP_ID"); // Application id from moralis.io
-Moralis.serverURL = "INSERT_SERVER_URL"; //Server url from moralis.io
+Moralis.initialize("NU6Zgw3Z1a3hqh5bRrSxD2LGbokEYyKIRdzHjLnP"); // Application id from moralis.io
+Moralis.serverURL = "https://yg5myylqwgy0.moralisweb3.com:2053/server"; //Server url from moralis.io
 
 async function login() {
     try {
         user = await Moralis.Web3.authenticate();
         console.log(user);
-        alert("User logged in")
+        alert("Logged in")
+        document.getElementById("login_button").style.display = "none";
+        document.getElementById("game").style.display = "block";
     } catch (error) {
         console.log(error);
     }
 }
 
+async function flip(side){
+    alert(side);
+}
+
+
 document.getElementById("login_button").onclick = login;
+document.getElementById("heads_button").onclick = function(){flip("heads")};
+document.getElementById("tails_button").onclick = function(){flip("tails")};
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
-## Coinflip dApp
+## :floppy_disk: Coin Flip dApp
 
-A simple dApp that allows the user to bet on the outcome of flipping a coin.
+A simple dApp which allows the user to bet on the outcome of a coin flip.
 
+
+:books: Libraries
+------
+Moralis, Web3, jQuery, Bootstrap
